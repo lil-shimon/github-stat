8e3faed0100ab0da7dda3e2911860b93bedecff0
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-07-03 22:44:46 +0900

    feat: 🎸 api url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,37 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useEffect } from "react";
+import { API_URL } from "../utils";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Github Stat</title>
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Github Stat
-        </h1>
+    useEffect(() => {
+        console.log("api", API_URL)
+    }, [])
 
-        <p className={styles.description}>
-        </p>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Github Stat</title>
+            </Head>
 
-        <div className={styles.grid}>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Github Stat
+                </h1>
+
+                <p className={styles.description}>
+                </p>
+
+                <div className={styles.grid}>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-      </footer>
-    </div>
-  )
+    )
 }
 
 export default Home
